658b9a7d774c6a4fd0320ead88a870339d8ac597
Tanvir Hassan Joy <99553203+Harmfulllll@users.noreply.github.com>
2024-05-16 02:05:43 +0600

    Final

---
+++ Calender/const.js
@@ -0,0 +1,30 @@
+const calendarEl = document.getElementById("calendar");
+
+const changeNow = () => new Date().getTime();
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+/*Add you calendar Key here */
+const googleApikey = "";
+
+/*Add you calendar ID here */
+const googleCalendarId_1 = "en.usa#holiday@group.v.calendar.google.com";
+/* update the event class to be styled by css */
+const gCalClass_1 = "cal-class-1";
+/*Add you calendar ID here */
+const googleCalendarId_2 = "en.uk#holiday@group.v.calendar.google.com";
+/* update the event class to be styled by css */
+const gCalClass_2 = "cal-class-2";
